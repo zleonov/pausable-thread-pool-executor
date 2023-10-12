c9ced5d65d876fab5fd981d8df92f909314b3310
eleonov <eleonov@DELL-PR-TR-3620>
2023-10-11 22:54:47 -0700

    Removed support for UninterruptibleThread

--- src/main/java/software/leonov/concurrent/ExecutorServices.java
+++ src/main/java/software/leonov/concurrent/ExecutorServices.java
@@ -1,6 +1,5 @@
 package software.leonov.concurrent;
 
-import static java.lang.Thread.NORM_PRIORITY;
 import static java.util.Objects.requireNonNull;
 import static software.leonov.concurrent.PausableThreadPoolExecutor.drainFully;
 
@@ -12,7 +11,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Static utility methods for {@link ExecutorService}s.
@@ -160,51 +158,6 @@ public final class ExecutorServices {
                 drainFully(queue, tasks);
             }
             return tasks;
-        }
-    }
-
-    /**
-     * Returns a new {@code ThreadFactory} that creates {@link UninterruptibleThread}s. Otherwise the returned factory
-     * behaves identically to {@link Executors#defaultThreadFactory()}.
-     * 
-     * @return a new {@code ThreadFactory} which creates {@link UninterruptibleThread}s
-     */
-    public static ThreadFactory uninterruptibleThreadFactory() {
-        return new DefaultUninterruptibleThreadFactory();
-    }
-
-    private static class DefaultUninterruptibleThreadFactory implements ThreadFactory {
-
-        private static final AtomicInteger poolCount = new AtomicInteger(1);
-
-        private final AtomicInteger threadCount = new AtomicInteger(1);
-        private final ThreadGroup group;
-        private final String prefix;
-
-        private DefaultUninterruptibleThreadFactory() {
-            final SecurityManager manager = System.getSecurityManager();
-
-            if (manager == null)
-                group = Thread.currentThread().getThreadGroup();
-            else
-                group = manager.getThreadGroup();
-
-            prefix = "pool-" + poolCount.getAndIncrement() + "-thread-";
-        }
-
-        @Override
-        public Thread newThread(final Runnable runnable) {
-            requireNonNull(runnable, "runnable == null");
-
-            final Thread t = new UninterruptibleThread(group, runnable, prefix + threadCount.getAndIncrement());
-
-            if (t.isDaemon())
-                t.setDaemon(false);
-
-            if (t.getPriority() != NORM_PRIORITY)
-                t.setPriority(NORM_PRIORITY);
-
-            return t;
         }
     }
 
